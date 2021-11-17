fe2d66840225e4c46562f536fe71564a3178b2c2
SamirRodrigues <emaildesamir@gmail.com>
2021-11-16 23:41:49 -0300

    Updates

---
+++ precache-manifest.630ba0a70d1ded56f3ffb8babcdae109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef467134bae36251e41984f70d70d7a6",
+    "url": "/Tours/index.html"
+  },
+  {
+    "revision": "2923ff527402ce14f97b",
+    "url": "/Tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2923ff527402ce14f97b",
+    "url": "/Tours/static/js/main.ef6c1459.chunk.js"
+  },
+  {
+    "revision": "3128dff8851166541ea2",
+    "url": "/Tours/static/js/runtime-main.83f56e71.js"
+  }
+]);
